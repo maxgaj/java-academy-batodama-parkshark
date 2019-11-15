9e077788d773720db92745598066acf172ed2865
Kas-Bashkim <g.bashkim@hotmail.fr>
2019-11-15 16:30:30 +0100

    CodeReview met Niels + Spring Security Dependencie deleted

--- war/src/main/java/be/cm/batodama/parkshark/war/Application.java
+++ war/src/main/java/be/cm/batodama/parkshark/war/Application.java
@@ -2,8 +2,12 @@ package be.cm.batodama.parkshark.war;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "be.cm.batodama.parkshark")
+@EnableJpaRepositories(basePackages = "be.cm.batodama.parkshark.domain")
+@EntityScan(basePackages = "be.cm.batodama.parkshark.domain")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
