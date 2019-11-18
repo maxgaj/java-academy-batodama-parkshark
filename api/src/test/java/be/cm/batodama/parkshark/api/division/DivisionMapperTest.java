c0636c36ee8aa6e241c7cd79e82b8784de618459
TomLeeBE <tom@tomlee.be>
2019-11-18 08:35:25 +0100

    Story DI1 in progress:created DivisionMapperTest

---
+++ api/src/test/java/be/cm/batodama/parkshark/api/division/DivisionMapperTest.java
@@ -0,0 +1,36 @@
+package be.cm.batodama.parkshark.api.division;
+
+import be.cm.batodama.parkshark.domain.division.Director;
+import be.cm.batodama.parkshark.domain.division.Division;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DivisionMapperTest {
+
+    @Test
+    void givenDivisionDto_whenMappingToDivision_thenAllFieldsAreEqual() {
+        DivisionDto divisionDto = new DivisionDto("Name",
+                "Original Name",
+                "Seymour",
+                "Skinner");
+        Division division = DivisionMapper.mapToDivision(divisionDto);
+        Assertions.assertEquals(division.getName(), divisionDto.name);
+        Assertions.assertEquals(division.getOriginalName(), divisionDto.originalName);
+        Assertions.assertEquals(division.getDirector().getFirstName(), divisionDto.firstName);
+        Assertions.assertEquals(division.getDirector().getLastName(), divisionDto.lastName);
+    }
+
+    @Test
+    void givenDivision_whenMappingToDivisionDto_thenAllFieldsAreEqual() {
+        Division division = new Division("Name",
+                "Original Name",
+                new Director("Seymour", "Skinner"));
+        DivisionDto divisionDto = DivisionMapper.mapToDivisionDto(division);
+        Assertions.assertEquals(division.getName(), divisionDto.name);
+        Assertions.assertEquals(division.getOriginalName(), divisionDto.originalName);
+        Assertions.assertEquals(division.getDirector().getFirstName(), divisionDto.firstName);
+        Assertions.assertEquals(division.getDirector().getLastName(), divisionDto.lastName);
+    }
+}
