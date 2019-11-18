47808722b170831400e2afca8cc59d9f22edf43f
maxgaj <maxime.gaj@gmail.com>
2019-11-18 16:41:28 +0100

    Integration test for Division getAll

--- api/src/test/java/be/cm/batodama/parkshark/api/division/DivisionControllerIntegrationTest.java
+++ api/src/test/java/be/cm/batodama/parkshark/api/division/DivisionControllerIntegrationTest.java
@@ -2,6 +2,7 @@ package be.cm.batodama.parkshark.api.division;
 
 import be.cm.batodama.parkshark.TestApiApplication;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,31 +10,53 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = TestApiApplication.class)
 @AutoConfigureMockMvc
-class DivisionControllerMockTest {
+class DivisionControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    private String validDivisionJson;
+
+    @BeforeEach
+    void setUp() {
+        validDivisionJson = asJsonString(new DivisionDto(
+                "Name",
+                "Original Name",
+                "Seymour",
+                "Skinner"));
+    }
+
     @Test
     @WithMockUser(authorities = "ROLE_MANAGER")
     void givenDivisionDto_whenCreatingDivision_thenReturnedDivisionDtoEqualsOriginal() throws Exception {
-        DivisionDto originalDivisionDto = new DivisionDto("Name",
-                "Original Name",
-                "Seymour",
-                "Skinner");
         mockMvc.perform(post("/divisions")
-                .content(asJsonString(originalDivisionDto))
+                .content(validDivisionJson)
                 .contentType(MediaType.APPLICATION_JSON)
                 .characterEncoding("UTF-8")
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
     }
+
+//    @Test
+//    void givenValidDivisionJson_whenCreatingAndGettinAllDivision_thenReturnCollectionWithProvidedValidDivision() throws Exception {
+//        MvcResult result = mockMvc.perform(post("/divisions")
+//                .content(validDivisionJson)
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .characterEncoding("UTF-8")
+//                .accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isCreated())
+//                .andReturn();
+//
+//
+//    }
 
     public static String asJsonString(final Object obj) {
         try {
