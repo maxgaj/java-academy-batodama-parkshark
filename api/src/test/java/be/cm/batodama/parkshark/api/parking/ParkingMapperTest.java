1c63d51a72ab8e98f37a3ac5c7c0148c70b6fb73
Kas-Bashkim <g.bashkim@hotmail.fr>
2019-11-18 16:34:16 +0100

    Finished with the tests of story PL1

--- api/src/test/java/be/cm/batodama/parkshark/api/parking/ParkingMapperTest.java
+++ api/src/test/java/be/cm/batodama/parkshark/api/parking/ParkingMapperTest.java
@@ -1,36 +1,39 @@
 package be.cm.batodama.parkshark.api.parking;
 
+import be.cm.batodama.parkshark.ApiTestApplication;
 import be.cm.batodama.parkshark.api.division.DivisionDto;
 import be.cm.batodama.parkshark.api.division.DivisionMapper;
 import be.cm.batodama.parkshark.domain.division.Director;
 import be.cm.batodama.parkshark.domain.division.Division;
+import be.cm.batodama.parkshark.domain.parking.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 class ParkingMapperTest {
 
     @Test
-    void givenDivisionDto_whenMappingToDivision_thenAllFieldsAreEqual() {
-        DivisionDto divisionDto = new DivisionDto("Name",
-                "Original Name",
-                "Seymour",
-                "Skinner");
-        Division division = DivisionMapper.mapToDivision(divisionDto);
-        Assertions.assertEquals(division.getName(), divisionDto.name);
-        Assertions.assertEquals(division.getOriginalName(), divisionDto.originalName);
-        Assertions.assertEquals(division.getDirector().getFirstName(), divisionDto.firstName);
-        Assertions.assertEquals(division.getDirector().getLastName(), divisionDto.lastName);
-    }
+    void givenParkingLot_whenMappingToParkingLotDto_thenAllFieldsAreEqual() {
 
-    @Test
-    void givenDivision_whenMappingToDivisionDto_thenAllFieldsAreEqual() {
-        Division division = new Division("Name",
-                "Original Name",
-                new Director("Seymour", "Skinner"));
-        DivisionDto divisionDto = DivisionMapper.mapToDivisionDto(division);
-        Assertions.assertEquals(division.getName(), divisionDto.name);
-        Assertions.assertEquals(division.getOriginalName(), divisionDto.originalName);
-        Assertions.assertEquals(division.getDirector().getFirstName(), divisionDto.firstName);
-        Assertions.assertEquals(division.getDirector().getLastName(), divisionDto.lastName);
+        ParkingLotMapper parkingLotMapper = new ParkingLotMapper();
+
+        ParkingLot parkingLot =
+                new ParkingLot("Test", ParkingLotCategory.UNDERGROUND, new Address("Street Test", "1",
+                        new PostCode("Post Test", "Leuven")), 50, new ParkingLotContactPerson("Niels", "niels@myemail.com", "484848484", "011848532",
+                            new Address("Street Test", "1",
+                                new PostCode("Post Test", "Leuven"))), 100);
+
+        ParkingLotDto parkingLotDto = parkingLotMapper.mapToParkingLotDto(parkingLot);
+        Assertions.assertEquals(parkingLotDto.parkingName, parkingLot.getParkingName());
+        Assertions.assertEquals(parkingLotDto.parkingCategory, parkingLot.getParkingCategory());
+        Assertions.assertEquals(parkingLotDto.parkingMaxSize, parkingLot.getParkingMaxSize());
+        Assertions.assertEquals(parkingLotDto.address.streetName, parkingLot.getAddress().getStreetName());
     }
 }
