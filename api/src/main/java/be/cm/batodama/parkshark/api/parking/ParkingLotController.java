ddca4cf422e39384d9902b8e2d9c65a338d8a949
Kas-Bashkim <g.bashkim@hotmail.fr>
2019-11-18 15:32:39 +0100

    Writing ParkingLot test

--- api/src/main/java/be/cm/batodama/parkshark/api/parking/ParkingLotController.java
+++ api/src/main/java/be/cm/batodama/parkshark/api/parking/ParkingLotController.java
@@ -9,7 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/parkinglot")
+@RequestMapping("/parkingLots")
 public class ParkingLotController {
 
     private final ParkingLotService parkingLotService;
---
+++ api/src/test/java/be/cm/batodama/parkshark/api/parking/ParkingControllerMockTest.java
@@ -0,0 +1,51 @@
+package be.cm.batodama.parkshark.api.parking;
+
+import be.cm.batodama.parkshark.api.ApiTestApplication;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest(classes = ApiTestApplication.class)
+@AutoConfigureMockMvc
+public class ParkingControllerMockTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void givenParkingLotDto_whenCreatingParkingLot_thenReturnedParkingLotDtoEqualsOriginal() throws Exception {
+
+        ParkingLotDto originalParkingLotDto = new ParkingLotDto("Test",
+                "UNDERGROUND",
+                new AddressDto("Street Test", "1", new PostCodeDto("Post Test", "Leuven")),
+                50,
+                1,
+                100);
+
+        mockMvc.perform(post("/parkingLots")
+                .content(asJsonString(originalParkingLotDto))
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+
+//        Mockito.when(parkingLotMapper.mapToParkingLot(originalParkingLotDto)).
+
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
