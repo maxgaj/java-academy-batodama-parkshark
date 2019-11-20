0a16c8c1bfc4bdadf4d0936f505c4e362606629a
DoebiMe <dannydoubbel@gmail.com>
2019-11-20 10:07:45 +0100

    changed test because time conflict, test was running so fast the date to test was equal to the date after fetching the data from the repository

--- api/src/test/java/be/cm/batodama/parkshark/api/member/SmallMemberDtoTest.java
+++ api/src/test/java/be/cm/batodama/parkshark/api/member/SmallMemberDtoTest.java
@@ -12,14 +12,15 @@ class SmallMemberDtoTest {
 
     @Test
     void makeMemberDtoTestFieldsEqualsConstructorParameters() {
+        LocalDateTime dateToInsert = LocalDateTime.now();
         SmallMemberDto smallMemberDto = new SmallMemberDto(
-                3, "daddy", "doebi", "kra235", "daddy@doebi.heaven", LocalDateTime.now());
+                3, "daddy", "doebi", "kra235", "daddy@doebi.heaven", dateToInsert);
         Assertions.assertEquals(smallMemberDto.id, 3);
         Assertions.assertTrue(smallMemberDto.email.contains("doebi.heaven"));
         Assertions.assertEquals(smallMemberDto.firstName, "daddy");
         Assertions.assertEquals(smallMemberDto.lastName, "doebi");
         Assertions.assertEquals(smallMemberDto.licencePlateNumber, "kra235");
-        Assertions.assertTrue(smallMemberDto.registrationDate.isBefore(LocalDateTime.now()));
+        Assertions.assertEquals(smallMemberDto.registrationDate,dateToInsert);
     }
 
 }
