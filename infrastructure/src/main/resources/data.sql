80d974f0c8f7f46769c1b15d8aeba1cccf5d92b3
TomLeeBE <tom@tomlee.be>
2019-11-20 09:57:25 +0100

    * updates schema.sql & data.sql with MembershipLevel

--- infrastructure/src/main/resources/data.sql
+++ infrastructure/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO BASE_USER VALUES (BASE_USER_SEQUENCE.NEXTVAL, 'member', '1234', 'ROLE_MEMBER');
-INSERT INTO MEMBER VALUES (1, 'member', 'parkshark', 'central street 42', '1000', 'Brussels', 'Belgium', 'member.parkshark@local.be', '0123456789', '1ABC123', 'Belgium', TO_DATE('2019/09/02 09:00:00', 'yyyy/mm/dd hh24:mi:ss'));
+INSERT INTO MEMBER VALUES (1, 'member', 'parkshark', 'central street 42', '1000', 'Brussels', 'Belgium', 'member.parkshark@local.be', '0123456789', '1ABC123', 'Belgium', TO_DATE('2019/09/02 09:00:00', 'yyyy/mm/dd hh24:mi:ss'), 'SILVER');
 
 INSERT INTO BASE_USER VALUES (BASE_USER_SEQUENCE.NEXTVAL, 'manager', '1234', 'ROLE_MANAGER');
 INSERT INTO MANAGER VALUES (2, 'manager', 'parkshark');
